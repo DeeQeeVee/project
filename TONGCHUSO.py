f092fb3ca16cf4302babe50bc333f31e3873e43a
IceUniverses <laiphuongthaovy@gmail.com>
2022-04-14 15:13:29 +0700

    Create new file: TONGCHUSO.*

---
+++ TONGCHUSO.py
@@ -0,0 +1,12 @@
+file_input = open("TONGCHUSO.inp", "r")
+file_output = open("TONGCHUSO.out", "w")
+
+n = file_input.readline()
+s = str(n)
+
+count = 0
+
+for i in s:
+        count += int(i)
+
+print(count, file = file_output, end = "")
