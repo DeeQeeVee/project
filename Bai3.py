0099744bad12a4212f29e3243bf638da4ad3fd32
IceUniverses <laiphuongthaovy@gmail.com>
2022-04-17 07:35:07 +0700

    Update Bai3.py

--- Bai3.py
+++ Bai3.py
@@ -8,9 +8,10 @@ n = int(n)
 #Given a string of characters s consisting of only lowercase English letters. Find the first position of a character that occurs only once in s. If there are no characters that satisfy the requirements of the problem, print -1.
 for line in file_input:
         line = line.strip()
+        
         for i in range(len(line)):
                 if(line.count(line[i]) == 1):
-                        print(i+1, file = file_output)
+                        print(i + 1, file = file_output)
                         break
         else:
                 print(-1, file = file_output)
