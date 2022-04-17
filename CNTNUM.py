4925d34e693fd12c19c7978cd62ec797aaf2fc24
IceUniverses <laiphuongthaovy@gmail.com>
2022-04-17 19:49:28 +0700

    Update CNTNUM.py

--- CNTNUM.py
+++ CNTNUM.py
@@ -5,11 +5,11 @@ a, b, c, d = map(int,file_input.read().split())
 m, n = c, d
 to = b - a + 1
 
-if m == n:
+if (m == n):
         F = [n]
 
 else:
-        bcnn = c*d
+        bcnn = c * d
 
         while c != d:
 
@@ -17,23 +17,25 @@ else:
                         c -= d
                 else:
                         d -= c
+                        
         bcnn = bcnn // c
 
-        if bcnn in [m,n]:
-                F= [min([m,n])]
+        if (bcnn in [m, n]):
+                F= [min([m, n])]    
 
         else:
-                F = [m,n,bcnn]
+                F = [m, n, bcnn]
 
 for i in F:
-        if a % i == 0:
+        if (a % i == 0):
                 t = b//i - a//i + 1
 
         else:
                 t = b//i - a//i
 
-        if len(F)>1 and i == bcnn:
+        if (len(F) > 1) and (i == bcnn):
                 t = -t
 
         to -= t
+
 print(to, file = file_output)
